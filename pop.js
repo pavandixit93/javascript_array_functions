9fca9b32e7aa0dee6db6e5104b29cfe0ef75e7e3
Pavan Dixit <pavandixit2012@gmail.com>
2018-09-18 12:15:48 +0530

    Update pop.js

--- pop.js
+++ pop.js
@@ -11,16 +11,21 @@
 
 var myArray = [1,2,3,4,5,6,7,8,9] // length of array = 9
 
+console.log("myArray is==="+myArray)
 console.log("Before pop()===="+myArray);
+
 var popArray = myArray.pop();
+
 console.log("After pop()===="+myArray); // 1,2,3,4,5,6,7,8
 console.log("Returned value==="+popArray); // 9
 
 var myArray = [1,2,3,4,5,6,7,8] // length of array = 8
 
 console.log("Before pop()===="+myArray); // 1,2,3,4,5,6,7,8
+
 var popArray = myArray.pop();
 var popArray = myArray.pop();
 var popArray = myArray.pop();
+
 console.log("After pop()===="+myArray); // 1,2,3,4,5
 console.log("Returned value==="+popArray); // 6
